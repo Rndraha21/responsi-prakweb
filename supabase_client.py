4e21c0a106f428a32934eef8e3b25925390b4b9f
Rndraha21 <robinndraha111@gmail.com>
2025-12-17 11:01:14 +0700

    update deployments

--- supabase_client.py
+++ supabase_client.py
@@ -1,12 +1,10 @@
 import os
 from dotenv import load_dotenv
 from supabase import create_client
-from httpx import Client as HTTPXClient
 
 load_dotenv()
 
-custom_http_client = HTTPXClient(http2=False)
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
-supabase = create_client(url, key, http_client=custom_http_client)
+supabase = create_client(url, key)
