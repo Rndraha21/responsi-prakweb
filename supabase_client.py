5160136dfed7d353e9066e15776e2a365e597d81
Rndraha21 <robinndraha111@gmail.com>
2025-12-21 15:28:45 +0700

    update

--- supabase_client.py
+++ supabase_client.py
@@ -1,10 +1,15 @@
 import os
 from dotenv import load_dotenv
 from supabase import create_client
+import httpx
 
 load_dotenv()
 
+transport = httpx.HTTPTransport(http2=False)
+
+http_client = httpx.Client(transport=transport, timeout=10.0)
+
 
 url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
-supabase = create_client(url, key)
+key = os.environ.get("SUPABASE_ANON_KEY")
+supabase = create_client(url, key, options={"http_client": http_client})
